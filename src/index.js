ca0f42ce93bcdf9a02a1f70c4c8993541ff0fb9f
ostanioni <ostanioni@gmail.com>
2019-05-29 12:56:00 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+let n = 1;
+const s = "hi";
+let sum = (a, b) => {
+    let c = a + b;
+    return c;
+};
+sum(5, 10);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+let n: number = 1
+const s: string = "hi"
+let sum = (a:number,b:number):number => {
+  let c: number = a + b
+  return c
+}
+sum(5,10)
