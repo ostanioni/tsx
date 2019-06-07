0035488073b83978f0db036bad0796e0719fa9b0
Sergio Ostanioni <ostanioni@gmail.com>
2019-06-07 20:10:58 +0700

    v 0.0.1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 // import { ThemeProvider } from 'styled-components';
 import Theme from 'components/MyContext';
+// import TrainingComponent from 'components/TrainingComponent';
+// import BackToTop from 'components/BackToTop';
 import NavIcons from 'components/NavIcons';
 function App() {
     return (React.createElement(Theme.Provider, { value: 'light' },
-        React.createElement(NavIcons, { name: 'globe', color: 'red', width: '10rem', onClick: (e) => { alert(e.target); } })));
+        React.createElement(NavIcons, { name: 'globe', color: 'red', width: '10rem', onClick: (e) => {
+                let html = document.querySelector('html');
+                if (html) {
+                    html.style.filter = "invert()";
+                }
+            } })));
 }
 export default App;
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React, {Provider} from 'react';
 // import { ThemeProvider } from 'styled-components';
 import Theme from 'components/MyContext';
-import TrainingComponent from 'components/TrainingComponent';
-import BackToTop from 'components/BackToTop';
+// import TrainingComponent from 'components/TrainingComponent';
+// import BackToTop from 'components/BackToTop';
 import NavIcons from 'components/NavIcons';
 
 
@@ -11,7 +11,12 @@ import NavIcons from 'components/NavIcons';
 function App() {
   return (
     <Theme.Provider value='light'>
-      <NavIcons name='globe' color='red' width='10rem' onClick={(e)=>{alert(e.target)}}/>
+      <NavIcons name='globe' color='red' width='10rem' onClick={(e)=>{
+        let html: HTMLHtmlElement | null = document.querySelector('html');
+        if(html){
+          html.style.filter="invert()";
+        }
+      }}/>
     </Theme.Provider>
   );
 }
